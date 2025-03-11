0e14ebdf1702252449cf7f72c1660808bacb3e01
Obed Murillo <obed.murillo@changecx.com>
2025-03-11 17:29:09 -0600

    adding session 2 init commit

--- RobotProject/src/main/java/org/example/session/Session02.java
+++ RobotProject/src/main/java/org/example/session/Session02.java
@@ -1,7 +1,18 @@
 package org.example.session;
 
+import javax.swing.*;
+import java.util.Random;
+
 public class Session02 {
     public static void main(String[] args) {
+        Random random = new Random();
+        Integer answer = random.nextInt(10) + 1;
+        String guess = JOptionPane.showInputDialog("Guess");
+        if(answer.toString() == guess) {
+            JOptionPane.showMessageDialog(null, "You did it");
+        } else {
+            JOptionPane.showMessageDialog(null, "Fail");
 
+        }
     }
 }
